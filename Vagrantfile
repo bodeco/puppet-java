5c9f67587c9b382a9ea9a3e3851b19b033016bd0
Dave Ingham <u0126564@thomsonreuters.com>
2014-11-05 15:13:13 -0800

    Provide support for Windows osfamily.
    * Convert from use of staging to archive for fetching artifacts.
    * Download and install Java installation EXE for windows.

--- Vagrantfile
+++ Vagrantfile
@@ -1,3 +1,5 @@
 require_relative 'spec/fixtures/modules/helper/lib/bodeco_module_helper/vagrant'
 
-vm :module => 'java', :box => 'oracle65-pe3.2.3'
+vm :hostname => 'java',    :module => 'java', :box => 'oracle65-pe3.2.3'
+
+vm :hostname => 'windows', :module => 'java', :box => 'windows2008r2', :cpu => 2, :gui => true, :type => :windows
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# Class: Oracle Java Installation for RHEL family
+# Class: Oracle Java Installation for MS Windows and for RHEL family.
 #
 class java (
   $dist    = 'jre',
@@ -7,31 +7,70 @@ class java (
   $update  = 60,
   $url     = 'http://download.oracle.com/otn-pub/java/jdk',
 ) {
+  include 'archive::staging'
 
-  $rpm_ver = "2000:1.${version}.0_${build}-fcs"
-  $rpm_name = "${dist}-${version}u${update}-linux-x64.rpm"
-  $source = "${url}/${version}u${update}-b${build}/${rpm_name}"
+  case $::osfamily {
+    'RedHat': {
+      $file_suffix     = 'linux-x64.rpm'
+      $rpm_ver         = "2000:1.${version}.0_${build}-fcs"
+      $provider        = rpm
+      $install_options = undef
+      $staging_path    = '/opt/staging'
+      $package_name    = $dist
+      $mode            = '0644'
+    }
+    'Windows': {
+      $file_suffix     = 'windows-x64.exe'
+      $rpm_ver         = undef
+      $provider        = windows
+      $install_options = '/s'
+      $staging_path    = $::staging_windir
+      $package_name    = "Java ${version} Update ${update} (64-bit)"
+      $mode            = '0740'
+    }
+    default: {
+      $file_suffix     = 'linux-x64.rpm'
+      $rpm_ver         = "2000:1.${version}.0_${build}-fcs"
+      $provider        = rpm
+      $install_options = undef
+      $staging_path    = '/opt/staging'
+      $package_name    = $dist
+      $mode            = '0644'
+    }
+  }
+
+  $artifact_name = "${dist}-${version}u${update}-${file_suffix}"
+  $source = "${url}/${version}u${update}-b${build}/${artifact_name}"
+  $local_artifact_file = "${staging_path}/${artifact_name}"
 
   # http://stackoverflow.com/questions/10268583/how-to-automate-download-and-installation-of-java-jdk-on-linux
-  staging::file { $rpm_name:
-    source      => $source,
-    curl_option => '-b "oraclelicense=accept-securebackup-cookie"',
-    wget_option => '--header "Cookie: oraclelicense=accept-securebackup-cookie"',
+  archive { $local_artifact_file:
+    source  => $source,
+    cleanup => false,
+    cookie  => 'oraclelicense=accept-securebackup-cookie',
   }
 
-  $rpm_file = "/opt/staging/java/${rpm_name}"
-
-  package { $dist:
-    ensure   => present,
-    source   => $rpm_file,
-    provider => rpm,
-    require  => Staging::File[$rpm_name],
+  file { $local_artifact_file:
+    mode    => $mode,
+    require => Archive[$local_artifact_file],
   }
 
-  exec { 'java_alternatives':
-    command     => 'alternatives --install /usr/bin/java java /usr/java/latest/bin/java 100',
-    path        => $::path,
-    refreshonly => true,
-    subscribe   => Package[$dist],
+  package { $package_name:
+    ensure          => present,
+    install_options => $install_options,
+    source          => $local_artifact_file,
+    provider        => $provider,
+    require         => File[$local_artifact_file],
+  }
+
+  # Post installation steps.
+  if $::osfamily != 'Windows'
+  {
+    exec { 'java_alternatives':
+      command     => 'alternatives --install /usr/bin/java java /usr/java/latest/bin/java 100',
+      path        => $::path,
+      refreshonly => true,
+      subscribe   => Package[$package_name],
+    }
   }
 }
